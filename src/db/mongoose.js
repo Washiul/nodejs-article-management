196e9a29acecb1128d68ad0243c9f7efa751b64b
Washiul <washi.washi786@gmail.com>
2022-05-12 15:13:06 +0530

    mongodb url removed

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const uri = "mongodb+srv://root:kanduri786@cluster0.6pb3d.mongodb.net/user-management?retryWrites=true&w=majority";
+const uri = "";
 mongoose.connect( uri )
 .then((data)=>console.log('Connected successfully'))
 .catch((e)=>console.log(e));
