684b8ecfb8afe23c4489636b154a324f95b68fe6
Washiul <washi.washi786@gmail.com>
2022-05-12 18:51:56 +0530

    check

--- src/index.js
+++ src/index.js
@@ -20,53 +20,53 @@ const partialPath = path.join(__dirname, '../templates/partials');
 const app = express();
 const port = process.env.PORT|3000;
 console.log('yehhh');
-// app.set( 'view engine', 'hbs' );
-// app.set( 'views', viewPath );
-// hbs.registerPartials( partialPath ); 
+app.set( 'view engine', 'hbs' );
+app.set( 'views', viewPath );
+hbs.registerPartials( partialPath ); 
 
-// hbs.registerHelper('isTrue', function (a) {
-//   return a?true:false;
-// });
+hbs.registerHelper('isTrue', function (a) {
+  return a?true:false;
+});
 
-// hbs.registerHelper('isEqual', function (a,b) {
-//   return a == b;
-// });
+hbs.registerHelper('isEqual', function (a,b) {
+  return a == b;
+});
 
-// hbs.registerHelper('toString', function (a) {
-// 	if( a ){
-// 		return a.toString('base64');
-// 	}
-//   	return '';
-// });
+hbs.registerHelper('toString', function (a) {
+	if( a ){
+		return a.toString('base64');
+	}
+  	return '';
+});
 
-// app.use(express.static(publicDirectoryPath));
-// // app.use(cookieParser());
-// // for parsing application/json
-// // app.use(bodyParser.json()); 
-// app.use(express.json())
-// // for parsing application/xwww-form-urlencoded
-// app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(express.static(publicDirectoryPath));
+// app.use(cookieParser());
+// for parsing application/json
+// app.use(bodyParser.json()); 
+app.use(express.json())
+// for parsing application/xwww-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true })); 
 
-// app.use(session({
-// 	secret: 'my first project',
-// 	resave: true,
-// 	saveUninitialized: true,
-// }));
-// app.use(flash());
-// app.use(passport.initialize());
-// app.use(passport.session());
-// passport.use(new LocalStrategy({usernameField: 'email'}, User.authenticate()));
-// passport.serializeUser(User.serializeUser());
-// passport.deserializeUser(User.deserializeUser());
+app.use(session({
+	secret: 'my first project',
+	resave: true,
+	saveUninitialized: true,
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy({usernameField: 'email'}, User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-// app.use((req, res, next)=>{
-// 	res.locals.user = req.user;
-// 	res.locals.success = req.flash('success');
-// 	res.locals.error = req.flash('error');
-// 	next();
-// });
+app.use((req, res, next)=>{
+	res.locals.user = req.user;
+	res.locals.success = req.flash('success');
+	res.locals.error = req.flash('error');
+	next();
+});
 
-// app.use( userRouter );
+app.use( userRouter );
 // app.use( blogRouter );
  
 app.listen( port, () => console.log( 'Server running on port '+port ) );
