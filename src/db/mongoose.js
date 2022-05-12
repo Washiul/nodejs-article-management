6ad30dd9d602af1f082eee59f1d4a3d95ef25344
Washiul <washi.washi786@gmail.com>
2022-05-12 16:29:27 +0530

    Adding env variable

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const uri = "";
+const uri = process.env.MONGODB_URL;
 mongoose.connect( uri )
 .then((data)=>console.log('Connected successfully'))
 .catch((e)=>console.log(e));
